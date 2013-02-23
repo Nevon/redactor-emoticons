e68ee791c90d3124cc91716fe6486c73abb02bcf
nevon <tommy.brunn@gmail.com>
2013-02-23 21:52:44 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,61 @@
+#Redactor-emoticons
+
+Redactor-emoticons is a plugin for the WYSIWYG editor [Redactor](http://imperavi.com/redactor/).
+
+##Installation
+
+Include redactor-emoticons.js in your markup:
+
+    <script src="redactor-emoticons.js"></script>
+
+Add the plugin to the `plugins` option and your emoticons to the `emoticons` option when you initialize Redactor:
+
+    $('#redactor-editor').redactor({
+        'plugins' : ['emoticons'],
+        'emoticons' : [
+            {
+                'name': 'Happy',
+                'src' : 'img/face-smile.png',
+                'shortcode' : ':)'
+            },
+            {
+                'name': 'Sad',
+                'src' : 'img/face-sad.png',
+                'shortcode' : ':('
+            },
+        ]
+    });
+
+`name` is simply used as an ALT-attribute.
+`src` is the URL to use to display the emoticon.
+`shortcode` - To quickly add an emoticon, type the corresponding shortcode, select it and press the emoticon button. Any selected text that matches a shortcode will be replaced with the corresponding image.
+
+##Usage
+
+Click on the emoticon button to get a modal window with all your emoticons.
+
+If you select a piece of text and press the emoticon button, any emoticons matching `shortcode` will be replaced with the appropriate image.
+
+##Translation
+
+The plugin only makes use of a single string, which is displayed as the modal window title as well as if you hover the emoticon button.
+
+By default this string is "Insert emoticon". If you want to replace it, add `emoticons` to your translation array. Ex:
+
+    var RELANG = {};
+    RELANG['sv'] = {
+        //Standard Redactor strings
+        emoticons : 'Lägg till smiley'
+    ]
+
+##License
+
+Copyright (c) 2013 Tommy Brunn (tommy.brunn@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  
+    
---
+++ redactor-emoticons.js
@@ -0,0 +1,73 @@
+/*
+ * Redactor emoticon plugin.
+ * Copyright (c) 2013 Tommy Brunn (tommy.brunn@gmail.com)
+ * https://github.com/Nevon/redactor-emoticons
+ */
+
+if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
+RedactorPlugins.emoticons = {
+	init: function() {
+
+		if (typeof(RLANG.emoticons) == 'undefined') {
+			RLANG.emoticons = 'Insert emoticon';
+		}
+		
+		/*
+		 * Create the button. When clicked either show 
+		 * modal or replace smilies in selected text.
+		 */
+		this.addBtn('emoticons', RLANG.emoticons, function(redactor, event) {
+			if (redactor.getSelectedHtml().length === 0 || redactor.replaceSmileys(redactor) === 0) {
+				redactor.createModal(redactor);
+			}
+		});
+
+		//Add a separator before the button
+		this.addBtnSeparatorBefore('emoticons');
+
+		//Add icon to button
+		$('a.redactor_btn_emoticons').css({
+			backgroundImage: ' url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QIXFAkhU/45rAAAAlNJREFUSMftVT1oU1EYPefF16QxKkkKkZqWd2+wwQhWEAQnB9G1FQfRzVF0UxfxZ7CDs1IRHYq6VEVRRCg4iRWnUAdbMZT70lpTWtGKYPPSJ+9zeS0lWH3VSem3XO695/sO5+O75wJr8d8Ho4Acx9lL8gjJbhHZCGAGwIsgCAYmJiaqv8qN/abwpnQ6fZfkSZLPReS2iNyzLOstgJ0k+zOZjMzNzb1ctYJisZjyfX9YRF57nnd8enq63owpFAqFIAgekxwyxpxeVe+UUreUUoPh1iqVSnaTuni45pRS77XWvT+rY63Qmi4APbZtnwjJhuv1+lR7e3smvD9gWdY3rXVftVqdEZFTItIXmcCyrF6STyqVyqfwKA4gnkgkYgBA0g5z4wCQzWYfktyilNoaiQBAUURGliYhFtvT0tKSN8Z8BADXdZ96ntdmjDkDAOVy+buIjALoai60bgWCOIDG4mZ8fHwBwMJyQK1W+9yU4wFIRFIgIh9IqkVMPp9PrDRpS+NIahGZitqiZyLSA8DSWp+3bftBLpdLLgd0dnZu833/ndZ6l+M43QDS2Wy2HPUdUGs9IiI35+fnB5LJ5B0Au0kOApgVke0ADpI8Z4zp11oPicgr13UvRlUgJI+RvJRKpfa7rnsIwFEACyKiSI6KyA5jzDWt9VURSXued3nVXhR60H2SjwBcMca8AYBSqWQ3Go19QRBcCKG9ruvO/pHZOY6TI3mW5GERWU/yq4i0AagAuN7a2npjbGzM/ys3XcR2dHRsjsViG4IgmJ2cnPyy9hn9G/EDW0nlPvgF9JwAAAAASUVORK5CYII=)'
+		});
+
+		$('img').css({
+			'cursor' : 'pointer'
+		});
+	},
+	createModal: function(redactor) {
+		var modal = '<div id="emoticon_drawer"><ul style="margin: 0; padding: 10px 10px;">';
+
+		for (var i = 0; i < this.opts.emoticons.length; i++) {
+			modal += '<li style="display: inline; margin: 5px;"><img src="'+this.opts.emoticons[i].src+'" alt="'+this.opts.emoticons[i].name+'" title="'+this.opts.emoticons[i].shortcode+'" style="cursor:pointer;"></li>';
+		}
+
+		modal += '</ul></div>';
+
+		this.modalInit(RLANG.emoticons, modal, 300, function() {
+			$('#emoticon_drawer img').click(function() {
+				redactor.setBuffer();
+				redactor.insertHtml('<img src="'+$(this).attr('src')+'" alt="'+$(this).attr('alt')+'">');
+			});
+		});
+	},
+	/*
+	 * @param redactor Redactor instance
+	 * @return int The number of smilies replaced
+	 */
+	replaceSmileys: function(redactor) {
+		var html = redactor.getSelectedHtml();
+		var numberOfMatches = 0;
+
+		//Replace all smileys within selected text.
+		for (var i = 0; i < this.opts.emoticons.length; i++) {
+			var smileyStr = (this.opts.emoticons[i].shortcode+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+			var pattern = RegExp('('+smileyStr+')', 'g');
+			numberOfMatches += (html.match(pattern) || []).length;
+			html = html.replace(pattern, '<img src="'+this.opts.emoticons[i].src+'" alt="'+this.opts.emoticons[i].name+'">');
+		}
+
+		redactor.insertHtml(html);
+
+		return numberOfMatches;
+	}
+}
